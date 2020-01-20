86e99caf5ab0c181bfe18cec04cefaee4e2fe669
Jennifer <Jennifer@MacBook-Air-de-Jennifer.local>
2020-01-20 11:06:25 +0100

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContainerComponent } from './container/container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: ContainerComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
+  { 
+    path: 'Formulaire',
+    loadChildren: () => import('./form/form.module').then(mod => mod.FormModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
